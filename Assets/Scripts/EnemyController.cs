6cab8563f4f7ab37809d986785d2986335c56f3e
Waldek <waldek.bus@gmail.com>
2023-11-05 12:09:41 +0100

    Tweeks

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 2.0f;
     protected Rigidbody enemyRigidBody;
     protected GameObject player;
 
